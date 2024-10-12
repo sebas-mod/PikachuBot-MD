f7cb93f6edab7161c8794a20f9ecb2946881161f
sebas-mod <luisrojas30921@gmail.com>
2024-10-12 01:44:15 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ import axios from 'axios'
 global.owner = [
   ['5491140951814', 'Sebas-MD⁩', true],
   ['5493865860050', 'keni', true],
-  ['50242073893'], 'jostin', true],
+  ['50242073893', 'jostin', true],
   [''],
   [''],
   [''],
