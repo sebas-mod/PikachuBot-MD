5f28917136a11a82e106a34ad732c76f586e4273
sebas-mod <luisrojas30921@gmail.com>
2024-10-23 01:22:28 -0300

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,12 +1,16 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*', mentions: [m.sender] }, { quoted: estilo })
-await conn.groupLeave(id)}
-handler.help = ['salir']
-handler.tags = ['owner']
-handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i
+let chat = global.db.data.chats[m.chat]
+chat.welcome = false
+await conn.reply(id, `🚩 *PikachuBot-MD* Abandona El Grupo, hasta la proxima negros👋`) 
+await conn.groupLeave(id)
+try {  
+chat.welcome = true
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
+handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
 handler.group = true
 handler.rowner = true
-
 export default handler
