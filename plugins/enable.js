79144543b8a409ac21995167e6221fe865c8e79f
sebas-mod <luisrojas30921@gmail.com>
2024-10-24 01:49:21 -0300

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -398,7 +398,7 @@ default:
 if (!/[01]/.test(command)) return await conn.reply(m.chat, luffyopcion, m, rcanal);
 throw false;
 }
-conn.reply(m.chat, `🍟  *La Función ${type} Se Ha ${isEnable ? 'Activado' : 'Desactivado'} En Este ${isAll ? 'Bot' : isUser ? '' : 'Chat.'}*`, m, rcanal, )
+ conn.sendMessage(m.chat, {text: `🗂️ 𝐎𝐏𝐂𝐈𝐎𝐍: ${type}\n🎚️ 𝐄𝐒𝐓𝐀𝐃𝐎: ${isEnable ? 'ACTIVADO' : 'DESACTIVADO'}\n📣 𝐏𝐀𝐑𝐀: ${isAll ? 'ESTE BOT' : isUser ? '' : 'ESTE CHAT'}`}, {quoted:  m, rcanal,});
 
 }
 handler.help = ['en', 'dis'].map((v) => v + 'able <option>');
